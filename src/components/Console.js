0f53dc78c755967219679cde64a5cfd2018b8f9b
VladimirSazanakov <sazanakov@inbox.ru>
2024-01-21 02:56:51 +0700

    console line add

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import styled from 'styled-components';
+import Flex from "./Flex";
+import Line from "./Line";
 
 const StyledConsole = styled.textarea`
 width: 100%;
@@ -14,8 +17,25 @@ resize: none;
 }
 `
 
-const Console = (props) => {
-  return <StyledConsole {...props} />
+const Console = ({ color, ...props }) => {
+
+  const [lines, setLines] = useState(['/homes/Vladimir>']);
+
+  const onKeyPress = e => {
+    if (e.keyCode == 13) {
+      setLines([...lines, '/homes/Vladimir>']);
+    }
+  }
+
+  return (
+    <Flex>
+      <Flex direction='column' margin="0 10px">
+        {lines.map(line => <Line color={color}>{line}</Line>)}
+      </Flex>
+      <StyledConsole onKeyDown={onKeyPress} color={color} {...props} />
+
+    </Flex>
+  )
 };
 
 export default Console;
