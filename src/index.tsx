d39fab4e71bebb86d06f8a099db001549e012053
VladimirSazanakov <sazanakov@inbox.ru>
2024-01-21 01:08:24 +0700

    Title and Flex

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}`
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Global />
     <App />
   </React.StrictMode>
 );
